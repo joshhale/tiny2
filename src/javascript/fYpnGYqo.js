69a84e31c9d56e2ace4b4b17fa181cf02808c843
User3 <user3@pentesting.semmle.net>
2018-03-20 16:45:06 +0000

    Add function to ./src/javascript/fYpnGYqo.js

--- src/javascript/fYpnGYqo.js
+++ src/javascript/fYpnGYqo.js
@@ -43,3 +43,11 @@ function XwYpv() {
     console.log(x);
 }
 // End of generated code
+
+// Start of generated code
+
+function jTBbLJp() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
