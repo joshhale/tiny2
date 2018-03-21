bcfaf7fcf5cde8915ccfd2f71d27fb932da7712c
User3 <user3@pentesting.semmle.net>
2018-03-21 23:27:04 +0000

    Add ./src/javascript/OUIexynL.js

---
+++ src/javascript/OUIexynL.js
@@ -0,0 +1,16 @@
+
+// Start of generated code
+
+function KMOOFVRJCa() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
+function dMrkWua() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
