c4d4e1c4cb294d699e92890b08eb1e0ff6f48ace
User4 <user4@pentesting.semmle.net>
2018-03-21 17:50:30 +0000

    Add ./src/python/LxgcOOHKd.py

---
+++ src/python/LxgcOOHKd.py
@@ -0,0 +1,30 @@
+
+# Start of generated code
+
+def ABbLOcZ(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
+def gsmaufY(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
+def SHHKtfn(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
+def WirLG(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
