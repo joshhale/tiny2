17b0faa415249b4c458c8dfcc94a5d3f706c5837
User5 <user5@pentesting.semmle.net>
2018-03-21 19:17:54 +0000

    Add function to ./src/python/cXGRX.py

--- src/python/cXGRX.py
+++ src/python/cXGRX.py
@@ -10,6 +10,14 @@ def wWpWAEle(arg):
 
 # Start of generated code
 
+def TQMtP(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
 def MUinJS(arg):
     print 'randstring0 ' + arg
 
