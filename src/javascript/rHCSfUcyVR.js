ff8da58626966a5aa7316f2264752405abf26a0b
User5 <user5@pentesting.semmle.net>
2018-03-21 06:22:42 +0000

    Add ./src/javascript/rHCSfUcyVR.js

---
+++ src/javascript/rHCSfUcyVR.js
@@ -0,0 +1,24 @@
+
+// Start of generated code
+
+function AExEKqBh() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
+function QOttfjuUZ() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
+function wtjETSJj() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
