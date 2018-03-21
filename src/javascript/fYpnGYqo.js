1584fcb61510ad7a72b44fc51080fa953eb72b19
User5 <user5@pentesting.semmle.net>
2018-03-21 05:33:06 +0000

    Delete function(s) from ./src/javascript/fYpnGYqo.js

--- src/javascript/fYpnGYqo.js
+++ src/javascript/fYpnGYqo.js
@@ -18,18 +18,4 @@ function wqGSrQEKp() {
 // End of generated code
 
 
-// Start of generated code
-
-function tDnGtoeB() {
-    var x = ""; // Unused local variable
-}
-// End of generated code
-
-// Start of generated code
-
-function NafyDsFnPx() {
-    var x = "";
-    console.log(x);
-}
-// End of generated code
 
