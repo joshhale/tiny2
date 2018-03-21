9c017ffe18a8f57481123d0557f3efe0d5b6dc4f
User1 <user1@pentesting.semmle.net>
2018-03-21 18:29:04 +0000

    Add ./src/python/oqYASoAWRm.py

---
+++ src/python/oqYASoAWRm.py
@@ -0,0 +1,30 @@
+
+# Start of generated code
+
+def yKtzdYRd(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
+def dvHltCa(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
+def aQZAPmW(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
+def ObVwrOvqC(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
