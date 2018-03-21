714a430b4ef78b578a7a7fbfb6342f44fcf9c4f3
User5 <user5@pentesting.semmle.net>
2018-03-21 19:38:07 +0000

    Add function to ./src/python/cXGRX.py

--- src/python/cXGRX.py
+++ src/python/cXGRX.py
@@ -10,6 +10,13 @@ def wWpWAEle(arg):
 
 # Start of generated code
 
+def qiIDA(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
 def TQMtP(arg):
     print 'randstring0 ' + arg
     f = open(arg) # Unclosed file handle
