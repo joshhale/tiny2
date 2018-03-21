a98adb12736a94559c7012c55120311f734dd598
User5 <user5@pentesting.semmle.net>
2018-03-21 18:26:53 +0000

    Add ./src/python/cXGRX.py

---
+++ src/python/cXGRX.py
@@ -0,0 +1,15 @@
+
+# Start of generated code
+
+def NlsQPrpJT(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
+def wWpWAEle(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
