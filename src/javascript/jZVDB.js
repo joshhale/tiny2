27f664a816691afcfae3f792af05c4522390ab3e
User3 <user3@pentesting.semmle.net>
2018-03-21 07:49:25 +0000

    Add ./src/javascript/jZVDB.js

---
+++ src/javascript/jZVDB.js
@@ -0,0 +1,30 @@
+
+// Start of generated code
+
+function srwqyYIn() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
+
+// Start of generated code
+
+function SCqlDBKAno() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
+function eWFznebvfC() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
+function UndeNZMFZ() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
