c2af464a2f6f46642314ca6842530881ee26daf0
User1 <user1@pentesting.semmle.net>
2018-03-21 22:00:00 +0000

    Add function to ./src/python/otqvwr.py

--- src/python/otqvwr.py
+++ src/python/otqvwr.py
@@ -8,6 +8,14 @@ def HxPCqs(arg):
 
 # Start of generated code
 
+def WzTMVH(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
 def BhPYlU(arg):
     print 'randstring0 ' + arg
     f = open(arg) # Unclosed file handle
