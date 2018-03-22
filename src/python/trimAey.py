72b887f38d525b82104676019ee679d353134533
User4 <user4@pentesting.semmle.net>
2018-03-22 11:13:06 +0000

    Delete function(s) from ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -6,20 +6,6 @@ def ouSUH(arg):
 
 # End of generated code
 
-# Start of generated code
-
-def wlJhFOva(arg):
-    print 'randstring0 ' + arg
-
-# End of generated code
-
-# Start of generated code
-
-def cKFKYFpK(arg):
-    print 'randstring0 ' + arg
-    f = open(arg) # Unclosed file handle
-
-# End of generated code
 
 
 # Start of generated code
