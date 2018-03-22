8a4ba41b33c58822c4b6290e17369e608900be5f
User5 <user5@pentesting.semmle.net>
2018-03-22 19:07:12 +0000

    Add function to ./src/javascript/mwJWVWJ.js

--- src/javascript/mwJWVWJ.js
+++ src/javascript/mwJWVWJ.js
@@ -8,6 +8,14 @@ function KSVeTWZILv() {
 
 // Start of generated code
 
+function WuTdbGqb() {
+    var x = "";
+    console.log(x);
+}
+// End of generated code
+
+// Start of generated code
+
 function QKqZfRCYUQ() {
     var x = ""; // Unused local variable
 }
