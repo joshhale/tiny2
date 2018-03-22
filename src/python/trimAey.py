6b7d3aad38f3cfb75ea21178baf06e2b5a08f37c
User4 <user4@pentesting.semmle.net>
2018-03-22 21:18:24 +0000

    Add function to ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -10,3 +10,10 @@ def aRmxCx(arg):
     print 'randstring0 ' + arg
 
 # End of generated code
+
+# Start of generated code
+
+def OshrA(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
