1efcd65666a4f224c45ad714db981a6c774990a1
User4 <user4@pentesting.semmle.net>
2018-03-22 01:43:17 +0000

    Add function to ./src/javascript/DbsPtCK.js

--- src/javascript/DbsPtCK.js
+++ src/javascript/DbsPtCK.js
@@ -8,6 +8,13 @@ function hoQtiL() {
 
 // Start of generated code
 
+function MvMAokowba() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
+
+// Start of generated code
+
 function ZnyZmUVy() {
     var x = "";
     console.log(x);
