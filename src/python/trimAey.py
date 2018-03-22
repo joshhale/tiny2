032f5508e864743f71f7ea2af003349e10ecb219
User4 <user4@pentesting.semmle.net>
2018-03-22 07:35:05 +0000

    Add ./src/python/trimAey.py

---
+++ src/python/trimAey.py
@@ -0,0 +1,29 @@
+
+# Start of generated code
+
+def ouSUH(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
+def gZzhQ(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
+# Start of generated code
+
+def RHFxZJo(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
+def RgGEG(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
