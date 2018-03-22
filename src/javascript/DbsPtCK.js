670060d792467c9bde6a597c15a614f8fd1465b6
User4 <user4@pentesting.semmle.net>
2018-03-22 00:56:22 +0000

    Add ./src/javascript/DbsPtCK.js

---
+++ src/javascript/DbsPtCK.js
@@ -0,0 +1,21 @@
+
+// Start of generated code
+
+function hoQtiL() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
+
+// Start of generated code
+
+function rSGhHV() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
+
+// Start of generated code
+
+function CtnCkp() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
