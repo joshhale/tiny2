a5786f04dce0ac998c1e0cf5647041ebef1cfebb
User4 <user4@pentesting.semmle.net>
2018-03-22 21:53:25 +0000

    Delete function(s) from ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -4,16 +4,3 @@
 
 
 
-# Start of generated code
-
-def aRmxCx(arg):
-    print 'randstring0 ' + arg
-
-# End of generated code
-
-# Start of generated code
-
-def OshrA(arg):
-    print 'randstring0 ' + arg
-
-# End of generated code
