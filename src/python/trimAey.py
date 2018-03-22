03b92aac91351d8466de09ab740cc06564d8bd62
User6 <user6@pentesting.semmle.net>
2018-03-22 13:53:21 +0000

    Add function to ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -6,6 +6,13 @@ def ouSUH(arg):
 
 # End of generated code
 
+# Start of generated code
+
+def ysVSAQdzC(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
+
 
 
 # Start of generated code
