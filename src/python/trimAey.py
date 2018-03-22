39284b8cc296fb60c4c691f3438059368f8ed4be
User4 <user4@pentesting.semmle.net>
2018-03-22 10:52:23 +0000

    Add function to ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -13,6 +13,14 @@ def wlJhFOva(arg):
 
 # End of generated code
 
+# Start of generated code
+
+def cKFKYFpK(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
 
 # Start of generated code
 
