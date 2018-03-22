b1b8145ce86e70cba721d473934f9a84fe3be4d1
User5 <user5@pentesting.semmle.net>
2018-03-22 20:39:00 +0000

    Add function to ./src/javascript/OUIexynL.js

--- src/javascript/OUIexynL.js
+++ src/javascript/OUIexynL.js
@@ -33,6 +33,13 @@ function dMrkWua() {
 
 // Start of generated code
 
+function fpeuoGKLcl() {
+    var x = ""; // Unused local variable
+}
+// End of generated code
+
+// Start of generated code
+
 function WTWpE() {
     var x = ""; // Unused local variable
 }
