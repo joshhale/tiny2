c01c961a5e773eb42e520a2f54dd8d4681d44fdd
User5 <user5@pentesting.semmle.net>
2018-03-23 08:42:01 +0000

    Delete function(s) from ./src/javascript/mwJWVWJ.js

--- src/javascript/mwJWVWJ.js
+++ src/javascript/mwJWVWJ.js
@@ -7,17 +7,3 @@ function KSVeTWZILv() {
 // End of generated code
 
 
-// Start of generated code
-
-function WuTdbGqb() {
-    var x = "";
-    console.log(x);
-}
-// End of generated code
-
-// Start of generated code
-
-function QKqZfRCYUQ() {
-    var x = ""; // Unused local variable
-}
-// End of generated code
