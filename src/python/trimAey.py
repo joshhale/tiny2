6251d660750b2661742f860c17520748194a6e70
User2 <user2@pentesting.semmle.net>
2018-03-23 07:00:48 +0000

    Add function to ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -11,3 +11,11 @@ def FTEYcgoqlh(arg):
     print 'randstring0 ' + arg
 
 # End of generated code
+
+# Start of generated code
+
+def NZAcLDfM(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
