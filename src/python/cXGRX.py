813219f9fb57add9c4b807ebd0e4e8944805278b
User4 <user4@pentesting.semmle.net>
2018-03-23 03:05:23 +0000

    Add function to ./src/python/cXGRX.py

--- src/python/cXGRX.py
+++ src/python/cXGRX.py
@@ -37,6 +37,14 @@ def qiIDA(arg):
 
 # End of generated code
 
+# Start of generated code
+
+def SYfAhxRD(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
 
 # Start of generated code
 
