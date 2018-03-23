322efe97d69a04c609e7598260b3869d5e2a36cf
User2 <user2@pentesting.semmle.net>
2018-03-23 10:29:40 +0000

    Add ./src/python/dWaVIAf.py

---
+++ src/python/dWaVIAf.py
@@ -0,0 +1,16 @@
+
+# Start of generated code
+
+def DpcFFBzQD(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
+# Start of generated code
+
+def hRtelpN(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
