ceea29a7b7e32a46a676e3b0a9e42664e5c1e945
User2 <user2@pentesting.semmle.net>
2018-03-23 15:28:02 +0000

    Add function to ./src/python/trimAey.py

--- src/python/trimAey.py
+++ src/python/trimAey.py
@@ -34,3 +34,11 @@ def xTeoH(arg):
 
 # End of generated code
 
+# Start of generated code
+
+def PGiXWeIUe(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
