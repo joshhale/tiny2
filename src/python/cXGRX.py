e225eaa2ad5a5c33b03366437926e3151991ed69
User6 <user6@pentesting.semmle.net>
2018-03-24 00:27:02 +0000

    Add function to ./src/python/cXGRX.py

--- src/python/cXGRX.py
+++ src/python/cXGRX.py
@@ -53,6 +53,14 @@ def TkhkG(arg):
 
 # End of generated code
 
+# Start of generated code
+
+def zuBtrUKIM(arg):
+    print 'randstring0 ' + arg
+    f = open(arg) # Unclosed file handle
+
+# End of generated code
+
 
 # Start of generated code
 
