0f677e6527b0e9f761d26502803a791644b9e29a
User6 <user6@pentesting.semmle.net>
2018-03-24 01:46:38 +0000

    Add function to ./src/python/gLETMSu.py

--- src/python/gLETMSu.py
+++ src/python/gLETMSu.py
@@ -10,3 +10,10 @@ def GQJIDZXZ(arg):
     f = open(arg) # Unclosed file handle
 
 # End of generated code
+
+# Start of generated code
+
+def KaKXTuZwG(arg):
+    print 'randstring0 ' + arg
+
+# End of generated code
